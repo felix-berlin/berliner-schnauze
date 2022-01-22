2b636cda67b043b28e434d651c6a95752b92e64f
Felix Scholze <felix@webshaped.de>
2022-01-22 23:44:14 +0100

    Add color scheme select

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <Table />
+  <div>
+    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <select v-model="$colorMode.preference">
+      <option value="system">
+        System
+      </option>
+      <option value="light">
+        Light
+      </option>
+      <option value="dark">
+        Dark
+      </option>
+    </select>
+    <Table />
+  </div>
 </template>
 
 <script>
