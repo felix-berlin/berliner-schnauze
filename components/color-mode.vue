e9c4ce6c0f1b17bcadd73fc560f4d15ce18c0c81
Felix Scholze <felix@webshaped.de>
2022-01-23 16:00:28 +0100

    Create Color Mode Component

---
+++ components/color-mode.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <select v-model="$colorMode.preference">
+      <option value="system">
+        System
+      </option>
+      <option value="light">
+        Light
+      </option>
+      <option value="dark">
+        Dark
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColorModeSwitch',
+
+  data () {
+    return {
+      modes: ''
+
+    }
+  }
+}
+</script>
