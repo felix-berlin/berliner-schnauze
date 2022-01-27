dd0447ec536cab7ed196ba561795be15980ed2ef
Felix Scholze <felix@webshaped.de>
2022-01-27 01:03:56 +0100

    Add loading spinner component

---
+++ components/loading-spinner.vue
@@ -0,0 +1,38 @@
+<template>
+  <transition name="fade">
+    <div v-show="show" class="c-loader">
+      <i class="c-loader__spinner c-loader__spinner--5" />
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'LoadingSpinner',
+
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data () {
+    return {
+
+    }
+  },
+
+  mounted () {
+
+  },
+
+  beforeDestroy () {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
