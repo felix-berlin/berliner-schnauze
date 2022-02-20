071434c4eec1420e53c4ef14f6f02a353be4b4e8
Felix Scholze <felix@webshaped.de>
2022-02-20 15:35:49 +0100

    Change title tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Berlinerisch zu Hochdeutsch Wörterbuch - Berliner Schnauze',
+    title: 'Berlinerisch zu Deutsch Wörterbuch - Lerne die Berliner Sprache!',
     htmlAttrs: {
       lang: 'de'
     },
