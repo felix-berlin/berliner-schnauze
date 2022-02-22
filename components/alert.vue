f57957f07588f6fbbfceec559ce0c22e76d36d69
Felix Scholze <felix@webshaped.de>
2022-02-22 17:17:03 +0100

    Add alert component

---
+++ components/alert.vue
@@ -0,0 +1,32 @@
+<template>
+  <component :is="element" :class="[componentClass, componentClass + '--' + type]" role="alert">
+    <slot>
+      Alert
+    </slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'AlertBanner',
+
+  props: {
+    element: {
+      type: String,
+      default: 'div',
+      require: true
+    },
+    type: {
+      type: String,
+      default: 'warning',
+      validator (value) {
+        return ['success', 'danger', 'warning', 'info'].includes(value)
+      }
+    },
+    componentClass: {
+      type: String,
+      default: 'c-alert'
+    }
+  }
+}
+</script>
