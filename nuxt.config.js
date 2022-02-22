a6bed110a9b1775b3a9339b450fee1ddea8ace8b
Felix Scholze <felix@webshaped.de>
2022-02-22 22:59:05 +0100

    Add dark-mode mixin
    - Changed global description
    - Add warning color

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,9 +25,9 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'format-detection', content: 'telephone=no' },
       { name: 'theme-color', content: '#F9F9F9' },
-      { hid: 'description', name: 'description', content: 'Berlinerisch ➡️ Hochdeutsch Wörterbuch. Keen Plan vom Berliner? Kein Problem, hier erfährste allet über die Berliner Sprache.' },
-      { hid: 'og:title', property: 'og:title', content: 'Berlinerisch zu Deutsch Wörterbuch - Lerne die Berliner Sprache!' },
-      { hid: 'og:description', property: 'og:description', content: 'Berlinerisch ➡️ Hochdeutsch Wörterbuch. Keen Plan vom Berliner? Kein Problem, hier erfährste allet über die Berliner Sprache.' },
+      { hid: 'description', name: 'description', content: 'Berlinerisch ➡️ Hochdeutsch Wörterbuch. Keen Plan vom Berliner Dialekt? Kein Problem, hier ist das aktuellste Berliner Sprache Wörterbuch.' },
+      { hid: 'og:title', property: 'og:title', content: 'Berlinerisch zu Hochdeutsch Wörterbuch - Berliner Dialekt' },
+      { hid: 'og:description', property: 'og:description', content: 'Berlinerisch ➡️ Hochdeutsch Wörterbuch. Keen Plan vom Berliner Dialekt? Kein Problem, hier ist das aktuellste Berliner Sprache Wörterbuch.' },
       { hid: 'og:image', property: 'og:image', content: '/brown-bear-roar.png' },
       { hid: 'og:url', property: 'og:url', content: process.env.NODE_ENV === 'production' ? process.env.BASE_URL : 'http://localhost:3000' },
       { hid: 'og:type', property: 'og:type', content: 'website' }
