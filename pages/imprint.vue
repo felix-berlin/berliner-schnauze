0f24ca31010cd9024793cc448a32edf256ccc3da
Felix Scholze <felix@webshaped.de>
2022-02-26 18:10:51 +0100

    Load content of legal pages via asyncData

--- pages/imprint.vue
+++ pages/imprint.vue
@@ -1,7 +1,6 @@
 <template>
   <main class="c-content o-legal-pages">
-    <LoadingSpinner :show="$fetchState.pending" />
-    <div v-if="status === 'publish'" v-html="content" />
+    <div v-if="imprint.status === 'publish'" v-html="imprint.content.rendered" />
   </main>
 </template>
 
@@ -9,22 +8,13 @@
 export default {
   name: 'ImprintPage',
 
-  data () {
-    return {
-      status: '',
-      content: ''
-    }
-  },
-
-  async fetch () {
-    return await fetch(`${this.$config.baseApiUrl}/wp-json/wp/v2/pages/4712`)
-      .then(res => res.json())
-      .then((res) => {
-        this.content = res.content.rendered
-        this.status = res.status
-      }).catch((error) => {
-        this.$sentry.captureException(new Error(error))
+  async asyncData ({ $axios, $config, $sentry }) {
+    const imprint = await $axios.$get(`${$config.baseApiUrl}/wp-json/wp/v2/pages/4712`)
+      .catch((error) => {
+        $sentry.captureException(error)
       })
+
+    return { imprint }
   },
 
   head () {
--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -1,7 +1,6 @@
 <template>
   <main class="c-content o-legal-pages">
-    <LoadingSpinner :show="$fetchState.pending" />
-    <div v-if="status === 'publish'" v-html="content" />
+    <div v-if="privacyPolicy.status === 'publish'" v-html="privacyPolicy.content.rendered" />
   </main>
 </template>
 
@@ -9,22 +8,13 @@
 export default {
   name: 'PrivacyPolicyPage',
 
-  data () {
-    return {
-      status: '',
-      content: ''
-    }
-  },
-
-  async fetch () {
-    return await fetch(`${this.$config.baseApiUrl}/wp-json/wp/v2/pages/4715`)
-      .then(res => res.json())
-      .then((res) => {
-        this.content = res.content.rendered
-        this.status = res.status
-      }).catch((error) => {
-        this.$sentry.captureException(new Error(error))
+  async asyncData ({ $axios, $config, $sentry }) {
+    const privacyPolicy = await $axios.$get(`${$config.baseApiUrl}/wp-json/wp/v2/pages/4715`)
+      .catch((error) => {
+        $sentry.captureException(error)
       })
+
+    return { privacyPolicy }
   },
 
   head () {
