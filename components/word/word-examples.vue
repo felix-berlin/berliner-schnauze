6b8c6c0fc03eae32bc49485022cd78365fabd764
Felix Scholze <felix@webshaped.de>
2022-03-27 22:34:57 +0200

    Fix wrong font-size for examples

--- components/word/word-examples.vue
+++ components/word/word-examples.vue
@@ -48,7 +48,7 @@ export default {
       default: 'c-word-list'
     },
     examples: {
-      type: [Object, Array, Boolean, null],
+      type: [Object, Array, Boolean, Symbol],
       default: null,
       required: true
     }
