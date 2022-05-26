3948256a6d7993be681b801cf902561a92972188
Felix Scholze <felix@webshaped.de>
2022-05-26 14:41:40 +0200

    Remove bear image preloading

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,8 +58,7 @@ export default {
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
-      { rel: 'preload', href: '/_nuxt/static/brown-bear-roar.webp', as: 'image' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' }
     ]
   },
 
