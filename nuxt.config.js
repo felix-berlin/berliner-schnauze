b33aae5659540bb2fb2f709a2c5ad716d630f7cd
Felix Scholze <felix@webshaped.de>
2022-05-31 02:31:01 +0200

    dev test

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,7 +80,8 @@ export default {
       lang: 'de',
       background_color: '#a9b3bd',
       start_url: process.env.NODE_ENV === 'production' ? process.env.BASE_URL : 'http://localhost:3000',
-      id: '?standalone=true'
+      id: '?standalone=true',
+      publicPath: process.env.NODE_ENV === 'production' ? 'https://pwa.berliner-schnauze.pages.dev/' : '/_nuxt/'
       // publicPath: process.env.NODE_ENV === 'production' ? process.env.BASE_URL + '/_nuxt/' : '/_nuxt/'
     },
     workbox: {
