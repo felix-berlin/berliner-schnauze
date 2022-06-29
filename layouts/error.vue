a4f626a277e17c805005c3b97f0731e2e2c1c6d2
Felix Scholze <felix@webshaped.de>
2022-06-30 00:03:20 +0200

    feat: add error page

---
+++ layouts/error.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="c-content o-error-page">
+    <h1 v-if="error.statusCode === 404" class="o-error-page__headline">
+      So war ditt nicht jedacht, die Seite is wech.
+    </h1>
+    <h1 v-else class="o-error-page__headline">
+      Ditt is ja affich, sowatt passiert sonst nie.
+    </h1>
+    <span class="o-error-page__icon"><ServerCrash :size="100" /></span>
+    <h2>Aktenzeichen: {{ error.statusCode }}</h2>
+
+    <NuxtLink :to="'/'" class="c-button c-button--back u-button-reset">
+      <ArrowLeft /> zum Wörterbuch
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+import { ServerCrash, ArrowLeft } from 'lucide-vue'
+
+export default {
+  name: 'ErrorPage',
+
+  components: {
+    ServerCrash,
+    ArrowLeft
+  },
+
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
