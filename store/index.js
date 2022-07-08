f5c1adf6f981dc5a6ada0610bd0b652e02eec0c1
Felix Scholze <felix@webshaped.de>
2022-07-08 20:16:05 +0200

    fix: word of the day loading status (4)

--- store/index.js
+++ store/index.js
@@ -157,17 +157,15 @@ export const actions = {
       })
   },
 
-  async loadWordOfTheDay ({ state, commit, $sentry }) {
-    if (!state.wordOfTheDay) {
-      commit('wordOfTheDayLoadingStatus', true)
-      return await fetch(`${this.$config.baseApiUrl}/wp-json/berliner-schnauze/v1/word-of-the-day`)
-        .then(res => res.json())
-        .then((data) => {
-          commit('updateWordOfTheDay', data)
-          commit('wordOfTheDayLoadingStatus', false)
-        })
-        .catch((err) => { $sentry.captureException(err) })
-    }
+  async loadWordOfTheDay ({ commit, $sentry }) {
+    commit('wordOfTheDayLoadingStatus', true)
+    return await fetch(`${this.$config.baseApiUrl}/wp-json/berliner-schnauze/v1/word-of-the-day`)
+      .then(res => res.json())
+      .then((data) => {
+        commit('updateWordOfTheDay', data)
+        commit('wordOfTheDayLoadingStatus', false)
+      })
+      .catch((err) => { $sentry.captureException(err) })
   },
 
   /**
