fc8adae86e812d84d8b24a0867dbc00fcacc8f23
Felix Scholze <felix@webshaped.de>
2022-07-15 14:31:37 +0200

    perf: reduced dom size

--- components/search-words.vue
+++ components/search-words.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="c-word-search" :class="[{ 'c-word-search--large': searchbarType === 'large', 'c-word-search--nav-search': searchbarType === 'nav-search' }, modifier]">
-    <!-- <transition name="fade-fast"> -->
     <div v-if="keyboardFocus" v-show="!showSearchBar && !$device.isMobileOrTablet" class="c-word-search__shortcut">
-      <span v-show="$device.isMacOS" class="c-word-search__command-icon-wrap">
+      <span v-if="$device.isMacOS" class="c-word-search__command-icon-wrap">
         <!-- eslint-disable-next-line -->
         <Command :size="12" class="c-word-search__command-icon" />
       </span>
-      <span v-show="$device.isWindows">Control</span>
+      <span v-if="$device.isWindows">Control</span>
       <span class="c-word-search__shortcut-combine">+</span>
       <span>K</span>
     </div>
-    <!-- </transition> -->
 
     <button
       aria-label="Wortsuche betätigen"
@@ -19,12 +17,14 @@
       :class="[{ 'c-word-search__search-button--right': (searchButtonPosition != 'left'), 'c-word-search__search-button--left': (searchButtonPosition != 'right'), 'has-searchbar': showSearchBar }, buttonModifier]"
       @click="buttonActions()"
     >
-      <span v-show="toggleShowAndClearIcon" class="c-button--center-icon">
-        <Search default-class="c-word-search__search-icon" />
-      </span>
-      <span v-show="!toggleShowAndClearIcon" class="c-button--center-icon">
-        <X />
-      </span>
+      <transition name="fade-fast" mode="out-in">
+        <span v-if="toggleShowAndClearIcon" key="search" class="c-button--center-icon">
+          <Search default-class="c-word-search__search-icon" />
+        </span>
+        <span v-if="!toggleShowAndClearIcon" key="del" class="c-button--center-icon">
+          <X />
+        </span>
+      </transition>
     </button>
 
     <transition-group v-show="showSearchBar" name="fade" class="c-word-search__search-wrap c-floating-label" :class="searchbarModifier" tag="div">
