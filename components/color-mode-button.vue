04eb60d62bd91e22aac4b0793f873f6b754a1502
Felix Scholze <felix@webshaped.de>
2022-07-16 16:46:22 +0200

    fix(color mode button): remove transition

--- components/color-mode-button.vue
+++ components/color-mode-button.vue
@@ -5,17 +5,15 @@
     :aria-label="ariaLabel"
     @click="changeColorMode"
   >
-    <transition-group name="fade-out-in" tag="span" class="c-button--center-icon">
-      <span v-show="$colorMode.value === 'dark'" key="dark" class="c-button--center-icon">
-        <Moon />
-      </span>
-      <span v-show="$colorMode.value === 'light'" key="light" class="c-button--center-icon">
-        <Sun />
-      </span>
-      <span v-show="$colorMode.value === 'system'" key="system" class="c-button--center-icon">
-        <Laptop2 />
-      </span>
-    </transition-group>
+    <span v-show="$colorMode.value === 'dark'" class="c-button--center-icon">
+      <Moon />
+    </span>
+    <span v-show="$colorMode.value === 'light'" class="c-button--center-icon">
+      <Sun />
+    </span>
+    <span v-show="$colorMode.value === 'system'" class="c-button--center-icon">
+      <Laptop2 />
+    </span>
   </button>
 </template>
 
