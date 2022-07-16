b85138d951f3d0d4d66d7d4b64879958bc119fc4
Felix Scholze <felix@webshaped.de>
2022-07-16 16:57:02 +0200

    feat(sort word direction toggle): add to index + styling

--- components/sort-word-direction-toggle.vue
+++ components/sort-word-direction-toggle.vue
@@ -1,15 +1,18 @@
 <template>
   <button
-    class="c-button c-button--center-icon"
+    type="button"
+    class="c-sort-word-direction-toggle c-button c-button--center-icon c-button--filter"
     :aria-label="'sortiere ' + ($store.state.wordSortDirection === 'asc' ? 'aufsteigend' : 'absteigend')"
     @click="$store.commit('updateWordSortDirection', $store.state.wordSortDirection === 'asc' ? 'desc' : 'asc')"
   >
     <transition name="fade-fast" mode="out-in">
-      <span v-if="$store.state.wordSortDirection === 'asc'" key="asc" class="c-button--center-icon">
+      <span v-if="$store.state.wordSortDirection === 'asc'" key="asc" class="c-sort-word-direction-toggle__button c-button--center-icon">
         <SortAsc />
+        <span>aufsteigend</span>
       </span>
-      <span v-if="$store.state.wordSortDirection === 'desc'" key="desc" class="c-button--center-icon">
+      <span v-if="$store.state.wordSortDirection === 'desc'" key="desc" class="c-sort-word-direction-toggle__button c-button--center-icon">
         <SortDesc />
+        <span>absteigend</span>
       </span>
     </transition>
   </button>
@@ -19,7 +22,7 @@
 import { SortAsc, SortDesc } from 'lucide-vue'
 
 export default {
-  name: 'SortWordDirection',
+  name: 'SortWordDirectionToggle',
 
   components: {
     SortAsc,
