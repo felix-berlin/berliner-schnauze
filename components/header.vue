d68963f027b8f9bc0798838421723d7da634decb
Felix Scholze <felix@webshaped.de>
2022-07-17 14:35:44 +0200

    feat(header): menu icon transition

--- components/header.vue
+++ components/header.vue
@@ -24,12 +24,14 @@
 
         <VMenu placement="bottom-end" :distance="13" strategy="fixed" container=".c-menu-nav" class="c-menu-nav__item c-menu-more">
           <button type="button" class="c-button c-menu-nav__item-button u-button-reset c-button--center-icon" aria-label="Website Menu Navigation">
-            <span v-if="$device.isDesktop" class="u-icon-untouchable u-icon-wrapper c-button--center-icon">
-              <Menu />
-            </span>
-            <span v-if="$device.isMobileOrTablet" class="u-icon-untouchable u-icon-wrapper c-button--center-icon">
-              <MoreVertical />
-            </span>
+            <transition name="fast" mode="out-in">
+              <span v-if="$device.isDesktop" key="desktop" class="u-icon-untouchable u-icon-wrapper c-button--center-icon">
+                <Menu />
+              </span>
+              <span v-if="$device.isMobileOrTablet" key="mobile" class="u-icon-untouchable u-icon-wrapper c-button--center-icon">
+                <MoreVertical />
+              </span>
+            </transition>
           </button>
 
           <template #popper>
