9bb796d918316a93310d8d3c87e9a4c691729ade
Felix Scholze <felix@webshaped.de>
2023-11-11 22:58:05 +0100

    feat: add Modal, Search and SearchModal

---
+++ src/components/Search.vue
@@ -0,0 +1,69 @@
+<template>
+  <div :id="id" class="c-search"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted } from "vue";
+import { PagefindUI } from "@pagefind/default-ui";
+
+export interface SearchProps {
+  id: string;
+}
+
+const props = defineProps<SearchProps>();
+
+let bundlePath = `${import.meta.env.BASE_URL}pagefind/`;
+
+if (import.meta.env.DEV) {
+  bundlePath = "/dist/pagefind/";
+  console.warn(
+    "If there are no search results, make sure you have run `npm run build` and that the `dist/pagefind` folder exists.",
+  );
+}
+
+/**
+ * Initialize Pagefind
+ *
+ * @return  {void}
+ */
+const initPagefind = (): void => {
+  new PagefindUI({
+    element: `#${props.id}`,
+    resetStyles: false,
+    showImages: false,
+    bundlePath,
+  });
+};
+
+/**
+ * Trigger search via keyboard
+ *
+ * @param   {KeyboardEvent}  event
+ *
+ * @return  {void}
+ */
+const triggerSearchViaKeyboard = (event: KeyboardEvent): void => {
+  const activeElement = document.activeElement?.tagName;
+  // If input or textarea is focused, do nothing
+  if (activeElement === "INPUT" || activeElement === "TEXTAREA") return;
+
+  if (event.key === "/" || event.key === ".") {
+    event.preventDefault();
+    const inputElement = document?.querySelector(`#${props.id} input`) as HTMLElement;
+    inputElement?.focus();
+  }
+};
+
+onMounted(() => {
+  initPagefind();
+
+  window.addEventListener("keydown", (event) => triggerSearchViaKeyboard(event));
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keydown", (event) => triggerSearchViaKeyboard(event));
+});
+</script>
+<style lang="scss">
+@use "@pagefind/default-ui/css/ui.css";
+</style>
