05e58984de3708a4af635b476a6821a73bc98f3f
Felix Scholze <felix@webshaped.de>
2023-11-15 00:06:09 +0100

    build: add missing configs for styleling and vitest

---
+++ stylelint.config.cjs
@@ -0,0 +1,46 @@
+/* eslint-env node */
+module.exports = {
+  customSyntax: "postcss-scss",
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recommended-scss",
+    "stylelint-config-recommended-vue",
+    "stylelint-config-recommended-vue/scss",
+    "stylelint-config-prettier-scss",
+  ],
+  overrides: [
+    {
+      customSyntax: "postcss-html",
+      files: ["*.vue", "**/*.vue"],
+      rules: {},
+    },
+  ],
+  // plugins: ['stylelint-declaration-block-no-ignored-properties'],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {
+    "max-line-length": null,
+    "selector-class-pattern": null,
+    "at-rule-empty-line-before": [
+      "always",
+      {
+        except: [
+          "after-same-name",
+          "blockless-after-blockless",
+          "first-nested",
+        ],
+        ignore: ["after-comment"],
+        ignoreAtRules: ["else"],
+      },
+    ],
+    "selector-type-no-unknown": [
+      true,
+      {
+        ignore: ["custom-elements", "default-namespace"],
+      },
+    ],
+    "no-descending-specificity": null,
+    "function-no-unknown": null,
+    "scss/at-extend-no-missing-placeholder": null,
+  },
+};
