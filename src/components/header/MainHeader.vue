c7f4a43a156d14ff4d23cf0470e921f28d128abb
Felix Scholze <felix@webshaped.de>
2023-11-18 01:55:58 +0100

    feat: remove mobile only icon

--- src/components/header/MainHeader.vue
+++ src/components/header/MainHeader.vue
@@ -22,30 +22,17 @@
             class="c-button c-menu-nav__item-button u-button-reset c-button--center-icon"
             aria-label="Website Menu Navigation"
           >
-            <transition name="fast" mode="out-in">
-              <span
-                v-if="device.isDesktop"
-                key="desktop"
-                class="u-icon-untouchable u-icon-wrapper c-button--center-icon"
-              >
-                <MenuIcon />
-              </span>
-              <!-- <span
-                v-if="device.isMobileOrTablet"
-                key="mobile"
-                class="u-icon-untouchable u-icon-wrapper c-button--center-icon"
-              >
-                <MoreVertical />
-              </span> -->
-            </transition>
+            <span class="u-icon-untouchable u-icon-wrapper c-button--center-icon">
+              <MenuIcon />
+            </span>
           </button>
 
           <template #popper>
             <SocialList list-modifier="c-social-list--mobile-dropdown" :hide-tooltips="true" />
             <ul class="c-menu-more__list u-list-reset">
-              <li class="c-menu-more__item">
+              <!-- <li class="c-menu-more__item">
                 <a :href="routeToWord(randomWord())"> Zufälliges Wort </a>
-              </li>
+              </li> -->
               <li
                 v-for="(item, index) in menuItems"
                 :key="index"
@@ -73,7 +60,6 @@
 
 <script setup lang="ts">
 import MenuIcon from "virtual:icons/lucide/menu";
-import MoreVertical from "virtual:icons/lucide/more-vertical";
 import { routeToWord, randomElement } from "@utils/helpers.ts";
 import { DeviceDetector } from "@utils/device.ts";
 import ColorModeToggle from "@components/ColorModeToggle.vue";
