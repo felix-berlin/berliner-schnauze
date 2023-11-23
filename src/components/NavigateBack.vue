0a7c950a37eaf2664a4ed2a26b005ee092dd189f
Felix Scholze <felix@webshaped.de>
2023-11-23 21:05:21 +0100

    feat(single-word): add NavigateBack component button

---
+++ src/components/NavigateBack.vue
@@ -0,0 +1,13 @@
+<template>
+  <button type="button" class="c-button c-button--back u-button-reset" @click="navigateBack">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+const navigateBack = () => {
+  window.history.back();
+};
+</script>
+
+<style scoped></style>
