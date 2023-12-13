63b09a7d4837d74511b9b98677de872d84947df5
Felix Scholze <felix@webshaped.de>
2023-12-13 01:06:53 +0100

    feat: basic toggle filter sidebar

---
+++ src/components/word-search/WordSearchFilterToggle.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    type="button"
+    class="c-word-search-filter-toggle c-button c-button--center-icon"
+    @click="toggleFilterSidebar"
+  >
+    <Filter />
+    <span>Filter</span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import Filter from "virtual:icons/lucide/filter";
+
+const toggleFilterSidebar = () => {
+  const filterSidebar = document.querySelector(".c-filter-search__filter");
+
+  if (!filterSidebar) {
+    return;
+  }
+
+  filterSidebar.classList.toggle("is-open");
+};
+</script>
+
+<style scoped></style>
--- src/components/word-search/WordSearchList.vue
+++ src/components/word-search/WordSearchList.vue
@@ -1,5 +1,6 @@
 <template>
-  <search class="c-word-search-list">
+  <search class="c-word-search-list" :class="props.cssClass">
+    <WordSearchFilterToggle />
     <SearchWords />
     <WordList />
   </search>
@@ -8,6 +9,7 @@
 <script setup lang="ts">
 import WordList from "@components/WordList.vue";
 import SearchWords from "@components/SearchWords.vue";
+import WordSearchFilterToggle from "@components/word-search/WordSearchFilterToggle.vue";
 import { $wordSearch } from "@stores/index";
 import type { Maybe } from "@ts_types/generated/graphql";
 import type { CleanBerlinerWord } from "@stores/index";
@@ -16,6 +18,7 @@ interface WordSearchListProps {
   words: CleanBerlinerWord[];
   availableLetterGroups: Maybe<string>[];
   wordTypes: Maybe<string>[];
+  cssClass: string;
 }
 
 const props = defineProps<WordSearchListProps>();
@@ -25,4 +28,6 @@ $wordSearch.setKey("wordTypes", props.wordTypes);
 $wordSearch.setKey("wordList", props.words);
 </script>
 
-<style scoped></style>
+<style lang="scss">
+@use "@styles/components/word-search-list";
+</style>
