dfb40a05c2b6e2a84f752839850d71d6003de535
Felix Scholze <felix@webshaped.de>
2024-01-24 19:07:54 +0100

    fix(pwa): remove fallback

--- astro.config.mjs
+++ astro.config.mjs
@@ -81,7 +81,7 @@ export default defineConfig({
       },
       workbox: {
         globDirectory: "dist",
-        navigateFallback: "/404",
+        // navigateFallback: "/404",
         globPatterns: ["**/*.{js,css,svg,png,jpg,jpeg,gif,webp,avif,woff,woff2,ttf,eot,ico}"],
       },
       devOptions: {
