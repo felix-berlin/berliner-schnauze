cb67c2d435e4322d32c18062f82ec58728b53468
Felix Scholze <felix@webshaped.de>
2024-02-25 21:46:33 +0100

    feat(IsWordOfTheDay): add more props

--- src/components/word/IsWordOfTheDay.vue
+++ src/components/word/IsWordOfTheDay.vue
@@ -4,12 +4,12 @@
     v-tooltip="{
       content: `${word}, ist das heutige Wort des Tages`,
       distance: 10,
-      placement: 'right',
+      placement: tooltipPlacement,
     }"
     class="c-word-of-the-day-crown"
     aria-hidden="true"
   >
-    <Crown />
+    <Crown :width="iconSize" :height="iconSize" />
   </span>
 </template>
 
@@ -23,9 +23,16 @@ import type { WordProperties } from "@ts_types/generated/graphql.ts";
 interface IsWordOfTheDayProps {
   wordId: number;
   word: WordProperties["berlinerisch"];
+  iconSize?: number;
+  tooltipPlacement?: "top" | "right" | "bottom" | "left";
 }
 
-const { wordId, word } = defineProps<IsWordOfTheDayProps>();
+const {
+  wordId,
+  word,
+  iconSize = 24,
+  tooltipPlacement = "right",
+} = defineProps<IsWordOfTheDayProps>();
 
 const wordOfTheDay = useStore($wordOfTheDay);
 
