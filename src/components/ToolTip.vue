0656100195300100ce6693b6f6cf25d4a3ccc602
Felix Scholze <felix@webshaped.de>
2024-02-25 21:46:55 +0100

    feat(ToolTip): add wrapper component

---
+++ src/components/ToolTip.vue
@@ -0,0 +1,11 @@
+<template>
+  <VTooltip client:only="vue" class="c-tooltip">
+    <slot />
+
+    <template #popper><slot name="content" /></template>
+  </VTooltip>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
