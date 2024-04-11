6ce9b95a51a815ad045f953b7f76863530302b42
Felix Scholze <felix@webshaped.de>
2024-04-11 21:53:19 +0200

    fix(nano stores dev tools): enable again

--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -1,23 +1,20 @@
 import type { App } from "vue";
 import FloatingVue from "floating-vue";
-
-// TODO: enable devtools when https://github.com/nanostores/logger/issues/18 is fixed
-
-// import { devtools } from "@nanostores/vue/devtools";
+import { devtools } from "@nanostores/vue/devtools";
 // import VueVirtualScroller from "vue-virtual-scroller";
 
-// import {
-//   $wordOfTheDay,
-//   $isDarkMode,
-//   $wordSearch,
-//   $filteredWordList,
-//   searchLength,
-//   $installPrompt,
-//   $showInstallButton,
-//   $showWordListFilterFlyout,
-//   $toastNotify,
-//   $activeFilterCount,
-// } from "../stores/index";
+import {
+  $wordOfTheDay,
+  $isDarkMode,
+  $wordSearch,
+  $filteredWordList,
+  searchLength,
+  $installPrompt,
+  $showInstallButton,
+  $showWordListFilterFlyout,
+  $toastNotify,
+  $activeFilterCount,
+} from "../stores/index";
 
 export default (app: App) => {
   app.use(FloatingVue, {
@@ -31,17 +28,17 @@ export default (app: App) => {
       },
     },
   });
-  // app.use(devtools, {
-  //   $wordOfTheDay,
-  //   $isDarkMode,
-  //   $wordSearch,
-  //   $filteredWordList,
-  //   searchLength,
-  //   $installPrompt,
-  //   $showInstallButton,
-  //   $showWordListFilterFlyout,
-  //   $toastNotify,
-  //   $activeFilterCount,
-  // });
+  app.use(devtools, {
+    $wordOfTheDay,
+    $isDarkMode,
+    $wordSearch,
+    $filteredWordList,
+    searchLength,
+    $installPrompt,
+    $showInstallButton,
+    $showWordListFilterFlyout,
+    $toastNotify,
+    $activeFilterCount,
+  });
   // app.use(VueVirtualScroller);
 };
