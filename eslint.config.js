ceb0ac2c7c9c3d033da6937b106c62173bf901fb
Felix Scholze <felix@webshaped.de>
2024-04-14 20:06:27 +0200

    build: add working eslint flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,32 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
-import jsxA11yRecommended from "eslint-plugin-jsx-a11y";
+// import jsxA11yRecommended from "eslint-plugin-jsx-a11y";
 import pluginVue from "eslint-plugin-vue";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 // TODO - add vuejs-accessibility/recommended
+import tsEslintParser from "@typescript-eslint/parser";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  // ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.recommendedTypeChecked,
   // ...tseslint.configs.stylisticTypeChecked,
   ...eslintPluginAstro.configs["flat/recommended"],
-  // jsxA11yRecommended.configs["flat/jsx-a11y-recommended"],
+  // jsxA11yRecommended.configs["flat/jsx-a11y-recommended"], // TODO - add when fixed: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/978
   ...pluginVue.configs["flat/recommended"],
-  eslintPluginPrettierRecommended,
   {
-    files: ["**/*.ts", "**/*.js", "**/*.astro", "**/*.vue"],
+    // files: ["**/*.ts", "**/*.js", "**/*.astro", "**/*.vue"],
     ignores: ["src/tests/*"],
-    // languageOptions: {
-    //   parserOptions: {
-    //     project: true,
-    //     tsconfigRootDir: import.meta.dirname,
-    //   },
-    // },
+    languageOptions: {
+      parserOptions: {
+        parser: tsEslintParser,
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
+        extraFileExtensions: [".vue"],
+      },
+    },
+  },
+  {
+    files: ["*.js"],
+    ...tseslint.configs.disableTypeChecked,
   },
 );
-
-// export default tseslint.config(
-//   eslint.configs.recommended,
-//   ...tseslint.configs.recommendedTypeChecked,
-//   {
-//     languageOptions: {
-//       parserOptions: {
-//         project: true,
-//         tsconfigRootDir: import.meta.dirname,
-//       },
-//     },
-//   },
-// );
