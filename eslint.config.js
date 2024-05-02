1bc64b186d8488df75cbba85ec0bccfe8e26d4c1
Felix Scholze <felix@webshaped.de>
2024-05-02 19:04:38 +0200

    build: update deps + eslint config changes

--- eslint.config.js
+++ eslint.config.js
@@ -3,30 +3,45 @@ import tseslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
 // import jsxA11yRecommended from "eslint-plugin-jsx-a11y";
 import pluginVue from "eslint-plugin-vue";
-// TODO - add vuejs-accessibility/recommended
+import pluginVueA11y from "eslint-plugin-vuejs-accessibility";
 import tsEslintParser from "@typescript-eslint/parser";
+import vueParser from "vue-eslint-parser";
+import url from "node:url";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.recommended,
+  // ...tseslint.configs.recommendedTypeChecked,
   // ...tseslint.configs.stylisticTypeChecked,
-  ...eslintPluginAstro.configs["flat/recommended"],
+  ...eslintPluginAstro.configs.recommended,
   // jsxA11yRecommended.configs["flat/jsx-a11y-recommended"], // TODO - add when fixed: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/978
   ...pluginVue.configs["flat/recommended"],
+  ...pluginVueA11y.configs["flat/recommended"],
   {
     // files: ["**/*.ts", "**/*.js", "**/*.astro", "**/*.vue"],
-    ignores: ["src/tests/*"],
+    ignores: [
+      "src/tests",
+      "**/coverage/",
+      "dist/",
+      "**/dev-dist/",
+      "**/node_modules/",
+      "**/__snapshots__/",
+      "src/types/generated/",
+    ],
     languageOptions: {
+      parser: vueParser,
       parserOptions: {
-        parser: tsEslintParser,
-        project: "./tsconfig.json",
-        tsconfigRootDir: import.meta.dirname,
-        extraFileExtensions: [".vue"],
+        parser: tseslint.parser,
+        project: "tsconfig.json",
+        tsconfigRootDir: __dirname,
+        extraFileExtensions: [".vue", ".astro"],
       },
     },
   },
-  {
-    files: ["*.js"],
-    ...tseslint.configs.disableTypeChecked,
-  },
+  // {
+  //   files: ["**/*.js"],
+  //   ...tseslint.configs.disableTypeChecked,
+  // },
 );
