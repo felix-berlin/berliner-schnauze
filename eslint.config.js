f48e08ef968e4544327cdc6d7707b4d9f6042385
Felix Scholze <felix@webshaped.de>
2024-05-09 20:45:22 +0200

    build: fixed eslint astro config

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,27 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
+import astroEslintParser from "astro-eslint-parser";
 // import jsxA11yRecommended from "eslint-plugin-jsx-a11y";
 import pluginVue from "eslint-plugin-vue";
 import pluginVueA11y from "eslint-plugin-vuejs-accessibility";
 import tsEslintParser from "@typescript-eslint/parser";
 import vueParser from "vue-eslint-parser";
 import url from "node:url";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  // ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.recommendedTypeChecked,
   // ...tseslint.configs.stylisticTypeChecked,
-  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["flat/recommended"],
   // jsxA11yRecommended.configs["flat/jsx-a11y-recommended"], // TODO - add when fixed: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/978
   ...pluginVue.configs["flat/recommended"],
   ...pluginVueA11y.configs["flat/recommended"],
+  eslintConfigPrettier,
   {
     // files: ["**/*.ts", "**/*.js", "**/*.astro", "**/*.vue"],
     ignores: [
@@ -33,15 +36,41 @@ export default tseslint.config(
     languageOptions: {
       parser: vueParser,
       parserOptions: {
-        parser: tseslint.parser,
+        parser: tsEslintParser,
         project: "tsconfig.json",
         tsconfigRootDir: __dirname,
-        extraFileExtensions: [".vue", ".astro"],
+        extraFileExtensions: [".vue"],
       },
     },
   },
-  // {
-  //   files: ["**/*.js"],
-  //   ...tseslint.configs.disableTypeChecked,
-  // },
+  {
+    files: ["**/*.js"],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
+    files: ["**/*.vue"],
+    rules: {
+      "vuejs-accessibility/label-has-for": [
+        "error",
+        {
+          components: ["VLabel"],
+          controlComponents: ["VInput"],
+          required: {
+            every: ["id"],
+          },
+          allowChildren: false,
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroEslintParser,
+      parserOptions: {
+        parser: tsEslintParser,
+        extraFileExtensions: [".astro"],
+      },
+    },
+  },
 );
