08b525dbd9137465e7afcebdde1069eefa4000cb
Felix Scholze <felix@webshaped.de>
2024-05-09 22:50:14 +0200

    test: remove broken test

--- src/tests/unit/utils/similarSoundingWords.test.ts
+++ src/tests/unit/utils/similarSoundingWords.test.ts
@@ -3,31 +3,9 @@ import { similarSoundingWords } from "@utils/wordHelper";
 import * as natural from "natural";
 import sinon from "sinon";
 
+const SoundEx = natural.SoundEx;
+
 describe("similarSoundingWords", () => {
-  it("calls natural.SoundEx.compare with the berlinerisch property of each word", ({ is }) => {
-    const originalCompare = natural.SoundEx.compare;
-    let calledWithArgs = null;
-
-    // Mock the function
-    natural.SoundEx.compare = (a, b) => {
-      calledWithArgs = [a, b];
-      return true;
-    };
-
-    const allWords = [
-      { id: "1", wordProperties: { berlinerisch: "word1" } },
-      { id: "2", wordProperties: { berlinerisch: "word2" } },
-    ];
-    const currentWord = { id: "1", wordProperties: { berlinerisch: "word1" } };
-
-    similarSoundingWords(allWords, currentWord);
-
-    assert.deepEqual(calledWithArgs, ["word2", "word1"]);
-
-    // Restore the original function
-    natural.SoundEx.compare = originalCompare;
-  });
-
   it("should filter out the current word and calculate similarity", () => {
     const allWords = [
       { id: 1, wordProperties: { berlinerisch: "word1" } },
@@ -42,14 +20,14 @@ describe("similarSoundingWords", () => {
     expect(result).toEqual([
       {
         word: allWords[0],
-        isSimilar: natural.SoundEx.compare(
+        isSimilar: new SoundEx().compare(
           allWords[0].wordProperties.berlinerisch,
           currentWord.wordProperties.berlinerisch,
         ),
       },
       {
         word: allWords[2],
-        isSimilar: natural.SoundEx.compare(
+        isSimilar: new SoundEx().compare(
           allWords[2].wordProperties.berlinerisch,
           currentWord.wordProperties.berlinerisch,
         ),
