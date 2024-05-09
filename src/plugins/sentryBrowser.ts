e77ebea73015f56a8bdd23f2baf6b5b61e79758a
Felix Scholze <felix@webshaped.de>
2024-05-09 14:29:26 +0200

    build(sentry): enable inp analytics

--- src/plugins/sentryBrowser.ts
+++ src/plugins/sentryBrowser.ts
@@ -9,7 +9,7 @@ Sentry.init({
   // Alternatively, use `process.env.npm_package_version` for a dynamic release version
   // if your build tool supports it.
   release: version,
-  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  integrations: [Sentry.browserTracingIntegration({ enableInp: true }), Sentry.replayIntegration()],
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
