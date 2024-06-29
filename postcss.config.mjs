6321625a138ccd5fa3c69cc5cc4c5febaa4bff4f
Felix Scholze <felix@webshaped.de>
2024-06-29 14:08:54 +0200

    build: remove common js config files

---
+++ postcss.config.mjs
@@ -0,0 +1,14 @@
+import autoprefixer from 'autoprefixer';
+import postcssPresetEnv from 'postcss-preset-env';
+
+/** @type {import('postcss-load-config').Config} */
+export default {
+  plugins: [
+    autoprefixer,
+    postcssPresetEnv({
+      features: {
+        "cascade-layers": false,
+      },
+    }),
+  ],
+};
