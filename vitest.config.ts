a5f43e12b7133b315db30cbe31047a2f02f11132
Felix Scholze <felix@webshaped.de>
2024-12-20 18:06:11 +0100

    ci: add test env vars

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,25 +1,78 @@
 /// <reference types="vitest" />
-import { getViteConfig } from "astro/config";
+import { getViteConfig, envField } from "astro/config";
 import { coverageConfigDefaults } from "vitest/config";
 
-export default getViteConfig({
-  test: {
-    include: ["src/tests/unit/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    exclude: ["src/tests/unit/__needsFix/*"],
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["src/tests/setup.ts", "@vitest/web-worker"],
-    coverage: {
-      include: ["src/**"],
-      exclude: [
-        "src/types/**",
-        "src/env.d.ts",
-        "src/pages/_app.ts",
-        "src/plugins/**",
-        "src/utils/supportedBrowsers.mjs",
-        ...coverageConfigDefaults.exclude,
-      ],
-      reportsDirectory: "./tests/unit/coverage",
+export default getViteConfig(
+  {
+    test: {
+      include: ["src/tests/unit/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+      exclude: ["src/tests/unit/__needsFix/*"],
+      globals: true,
+      environment: "jsdom",
+      setupFiles: ["src/tests/setup.ts", "@vitest/web-worker"],
+      coverage: {
+        include: ["src/**"],
+        exclude: [
+          "src/types/**",
+          "src/env.d.ts",
+          "src/pages/_app.ts",
+          "src/plugins/**",
+          "src/utils/supportedBrowsers.mjs",
+          ...coverageConfigDefaults.exclude,
+        ],
+        reportsDirectory: "./tests/unit/coverage",
+      },
     },
   },
-});
+  {
+    env: {
+      schema: {
+        WP_API: envField.string({
+          context: "client",
+          access: "public",
+          default: "https://berliner-schnauze.test/api",
+        }),
+        WP_REST_API: envField.string({
+          context: "client",
+          access: "public",
+          default: "https://berliner-schnauze.test/wp-json",
+        }),
+        WP_AUTH_REFRESH_TOKEN: envField.string({
+          context: "client",
+          access: "public",
+          default: "not-a-real-token",
+        }),
+        SUGGEST_WORD_FORM_ID: envField.string({
+          context: "client",
+          access: "public",
+          default: "1111",
+        }),
+        TURNSTILE_SITE_KEY: envField.string({
+          context: "client",
+          access: "public",
+          default: "not-a-real-key",
+        }),
+        SENTRY_PROJECT: envField.string({
+          context: "server",
+          access: "public",
+          default: "not-a-real-project",
+        }),
+        SENTRY_ORG: envField.string({
+          context: "server",
+          access: "public",
+          default: "not-a-real-org",
+        }),
+        SENTRY_ENVIRONMENT: envField.string({
+          context: "client",
+          access: "public",
+          default: "development",
+        }),
+        SENTRY_TRACES_SAMPLE_RATE: envField.number({
+          context: "client",
+          access: "public",
+          default: 0.1,
+        }),
+      },
+    },
+  },
+);
