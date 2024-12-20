9d6d520ad5ea691a770eca22aa5ca7657ac4b745
Felix Scholze <felix@webshaped.de>
2024-12-20 17:19:48 +0100

    build: fix missing env vars

--- src/services/sendMail.ts
+++ src/services/sendMail.ts
@@ -1,7 +1,7 @@
 import { fetchAPI } from "@services/fetchApi.ts";
 import type { SendEmailPayload, SendEmailInput } from "@ts_types/generated/graphql.ts";
 import type { FormData } from "@components/SuggestWordForm.vue";
-import { WP_REST_API, SUGGEST_WORD_FORM_ID } from "astro:env/client";
+import { WP_REST_API, SUGGEST_WORD_FORM_ID, WP_AUTH_REFRESH_TOKEN } from "astro:env/client";
 
 /**
  * Sends an email using the sendEmail mutation
@@ -59,7 +59,7 @@ export const sendEmailViaContactForm7 = async (formData: FormData): Promise<void
 
   await fetch(`${WP_REST_API}`, {
     headers: {
-      Authorization: `Bearer ${import.meta.env.WP_AUTH_REFRESH_TOKEN}`,
+      Authorization: `Bearer ${WP_AUTH_REFRESH_TOKEN}`,
     },
   })
     .then((response) => response.json())
@@ -69,7 +69,7 @@ export const sendEmailViaContactForm7 = async (formData: FormData): Promise<void
   await fetch(`${WP_REST_API}/contact-form-7/v1/contact-forms/${SUGGEST_WORD_FORM_ID}/feedback`, {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${import.meta.env.WP_AUTH_REFRESH_TOKEN}`,
+      Authorization: `Bearer ${WP_AUTH_REFRESH_TOKEN}`,
     },
     body: formInputs,
   }).then((response) => {
