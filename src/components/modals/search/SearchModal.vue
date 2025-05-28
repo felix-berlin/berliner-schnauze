2de5253c9f1b4ac492ee128b6a1041ccdc3fcb1d
Felix Scholze <felix@webshaped.de>
2025-05-28 21:37:41 +0200

    feat(search-modal): add NoSearchResults component and integrate into WordSearchList for improved user feedback

--- src/components/modals/search/SearchModal.vue
+++ src/components/modals/search/SearchModal.vue
@@ -3,7 +3,11 @@
   <div role="search" class="c-search-container">
     <SearchWords ref="searchWords" :auto-focus="true" />
 
+    <WordFilter />
+
     <SearchResultCount />
+
+    <NoSearchResults />
 
     <WordList
       class="c-word-list--small"
@@ -26,6 +30,8 @@ import SearchResultCount from "@components/word-search/SearchResultCount.vue";
 import ShortcutSelect from "@components/word-search/shortcuts/ShortcutSelect.vue";
 import ShortcutNavigating from "@components/word-search/shortcuts/ShortcutNavigating.vue";
 import ShortcutClose from "@components/word-search/shortcuts/ShortcutClose.vue";
+import NoSearchResults from "@components/word-search/NoSearchResults.vue";
+import WordFilter from "@components/word-search/WordFilter.vue";
 
 const SearchWords = defineAsyncComponent(() => import("@components/SearchWords.vue"));
 const WordList = defineAsyncComponent(() => import("@components/WordList.vue"));
--- src/components/word-search/WordSearchList.vue
+++ src/components/word-search/WordSearchList.vue
@@ -11,10 +11,7 @@
       <ShortcutNavigating />
     </div>
 
-    <div v-if="searchResultCount === 0" class="c-word-search-list__no-result">
-      <SearchX width="50" height="50" />
-      <p>Da biste anjemeiat. Keen Treffer.</p>
-    </div>
+    <NoSearchResults />
 
     <WordSuggestHint v-if="searchResultCount === 0" />
 
@@ -28,12 +25,12 @@ import WordList from "@components/WordList.vue";
 import SearchWords from "@components/SearchWords.vue";
 import WordSearchFilterToggle from "@components/word-search/WordSearchFilterToggle.vue";
 import { $wordSearch, $searchResultCount } from "@stores/index";
-import SearchX from "virtual:icons/lucide/search-x";
 import type { Maybe } from "@/gql/graphql.ts";
 import ShortcutSelect from "@components/word-search/shortcuts/ShortcutSelect.vue";
 import ShortcutNavigating from "@components/word-search/shortcuts/ShortcutNavigating.vue";
 import SearchResultCount from "@components/word-search/SearchResultCount.vue";
 import { useStore } from "@nanostores/vue";
+import NoSearchResults from "@components/word-search/NoSearchResults.vue";
 
 type WordSearchListProps = {
   availableLetterGroups: Maybe<string>[];
