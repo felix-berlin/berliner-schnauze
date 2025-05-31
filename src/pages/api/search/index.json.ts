833450cc4dfbc41bdf678230068c3b13d03ca2e9
Felix Scholze <felix@webshaped.de>
2025-05-31 17:21:43 +0200

    feat(api): search index comes with more filter data

--- src/pages/api/search/index.json.ts
+++ src/pages/api/search/index.json.ts
@@ -1,8 +1,12 @@
 import type { APIRoute } from "astro";
 import { fetchAllWords } from "@services/api.ts";
 import type { BerlinerWord } from "@/gql/graphql.ts";
+import Hypher from "hypher";
+import german from "hyphenation.de";
+import { countLetters, similarSoundingWords } from "@utils/wordHelper.ts";
 
-function makeOramaSearchIndex(node: BerlinerWord) {
+function makeOramaSearchIndex(node: BerlinerWord, allWords) {
+  const hypher = new Hypher(german);
   // At the moment, we are not using examples in the search index.
   //
   // const examples = Array.isArray(node.wordProperties?.examples)
@@ -28,6 +32,20 @@ function makeOramaSearchIndex(node: BerlinerWord) {
       berlinerisch: node.wordProperties?.berlinerisch,
       berolinismus: node.wordProperties?.berolinismus,
       translations,
+      syllablesCount: hypher.hyphenate(node.wordProperties.berlinerisch).length,
+      multipleMeanings: !!node.wordProperties?.alternativeWords,
+      characterLength: node.wordProperties?.berlinerisch?.length,
+      audioBerlinerisch: !!node.wordProperties?.berlinerischAudio,
+      audioExamples:
+        (node?.wordProperties?.examples ?? [])
+          .map((e) => e?.exampleAudio)
+          .filter((a) => !!a?.length).length > 0,
+      consonantsCount: countLetters(node.wordProperties?.berlinerisch).consonants,
+      vowelsCount: countLetters(node.wordProperties?.berlinerisch).vowels,
+      similarSoundingWords: !!similarSoundingWords(
+        allWords,
+        node.wordProperties?.berlinerisch,
+      ).find((word) => word.isSimilar),
     },
   };
 }
@@ -37,7 +55,7 @@ export type OramaSearchIndex = ReturnType<typeof makeOramaSearchIndex>;
 export const GET: APIRoute = async ({ params, request }) => {
   const allWords = await fetchAllWords();
 
-  const oramaSearchIndex = allWords.map(({ node }) => makeOramaSearchIndex(node));
+  const oramaSearchIndex = allWords.map(({ node }) => makeOramaSearchIndex(node, allWords));
 
   return new Response(JSON.stringify(oramaSearchIndex));
 };
