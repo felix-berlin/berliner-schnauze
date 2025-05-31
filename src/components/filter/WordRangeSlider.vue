19d2a875321b7ae7bd4fe02378b10c9d12324565
Felix Scholze <felix@webshaped.de>
2025-05-31 18:49:42 +0200

    feat: add WordRangeSlider

---
+++ src/components/filter/WordRangeSlider.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="c-word-range-slider">
+    <input :id="id" v-model="rangeValue" type="range" max="40" />
+    <label :for="id">{{ label }}</label>
+  </div>
+
+  <input
+    v-model="rangeValue"
+    type="number"
+    :aria-label="`Anzahl der ${label}`"
+    placeholder="Alle"
+    class="c-input c-input--number"
+    min="0"
+    max="40"
+  />
+
+  <button
+    v-if="typeof rangeValue !== 'undefined'"
+    type="button"
+    class="c-button c-button--center-icon"
+    @click="resetRange"
+  >
+    <RotateCcwIcon /> reset
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useId } from "vue";
+import { useVModel } from "@nanostores/vue";
+import { $wordSearch } from "@stores/index.ts";
+import RotateCcwIcon from "virtual:icons/lucide/rotate-ccw";
+
+const { rangeType, label } = defineProps<{
+  rangeType: "characterCount" | "consonantsCount" | "vowelsCount" | "syllablesCount";
+  label: string;
+}>();
+
+const rangeValue = useVModel($wordSearch, rangeType);
+
+const id = useId();
+
+const resetRange = () => {
+  rangeValue.value = undefined;
+};
+</script>
+
+<style scoped></style>
