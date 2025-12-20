c48f0e9d706d2caf84d7b5157ad6d9b48d9874c0
Felix Scholze <felix@webshaped.de>
2025-12-20 17:34:06 +0100

    feat: add selection tracking to WordTypeFilter dropdown

--- src/components/filter/WordTypeFilter.vue
+++ src/components/filter/WordTypeFilter.vue
@@ -10,6 +10,7 @@
       fallback-locale="en"
       class="c-word-type-filter__select"
       placeholder="Worttypen filtern"
+      @select="onSelect(value)"
     />
   </nav>
 </template>
@@ -17,10 +18,15 @@
 <script setup lang="ts">
 import { useStore, useVModel } from "@nanostores/vue";
 import { $wordSearch } from "@stores/index.ts";
+import { trackEvent } from "@utils/analytics";
 import Multiselect from '@vueform/multiselect';
 
 const wordSearch = useStore($wordSearch);
 const value = useVModel($wordSearch, 'activeWordTypeFilter');
+
+const onSelect = (value: string) => {
+  trackEvent("WordList", "Filter", `Word Type: ${value}`);
+};
 </script>
 
 <style lang="scss">
