6ab57e0d8cce6bdc7af3f852101fa3605d938005
Felix Scholze <felix@webshaped.de>
2026-06-26 13:48:30 +0200

    test: simplify AlertBanner component mock in SuggestWordForm tests

--- src/tests/unit/components/SuggestWordForm.test.ts
+++ src/tests/unit/components/SuggestWordForm.test.ts
@@ -37,16 +37,13 @@ vi.mock("@components/TurnStile.vue", () => ({
   },
 }));
 
-vi.mock("@components/AlertBanner.vue", async (importOriginal) => {
-  const actual = await importOriginal<typeof import("@components/AlertBanner.vue")>();
-  return {
-    ...actual,
-    default: {
-      name: "AlertBanner",
-      template: "<div class='mock-alert-banner'><slot /></div>",
-    },
-  };
-});
+vi.mock("@components/AlertBanner.vue", () => ({
+  __isTeleport: false,
+  default: {
+    name: "AlertBanner",
+    template: "<div class='mock-alert-banner'><slot /></div>",
+  },
+}));
 
 
 describe("SuggestWordForm.vue", () => {
